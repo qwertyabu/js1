0a0ee978796918fae4899b69e9e9a502ade80271
qwertyabu <monkey.famil4@gmail.com>
2025-01-09 11:26:59 +0300

    Add files via upload

---
+++ media.js
@@ -0,0 +1,34 @@
+let abu = prompt('ismingizn kiritin')
+alert(abu)
+console.log(abu);
+
+let admin = prompt('familiyangizini kiritin')
+alert(admin)
+console.log(admin);
+
+let two = prompt('yoshingizni kiritin')
+alert(two)
+console.log(two);
+
+let one = prompt('yahshikorgan faningiz')
+alert(one)
+console.log(one);
+
+let three = prompt('yashash joinngiz')
+alert(three)
+console.log(three);
+
+let four = 'malumotlar uchun rahmat'
+console.log(four)
+
+
+
+
+
+
+//let user = confirm("botsan");
+///alert(user);
+//let umid = prompt("botsan");
+//alert(umid);
+//let admin = promt("сколько тебе лет")
+//alert(`тебе реально DOLOR`)
